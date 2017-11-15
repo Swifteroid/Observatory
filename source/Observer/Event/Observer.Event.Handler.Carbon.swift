360072b7d22867746b98a3bffa4cda10baa4b7a5
Ian Bytchek <ianbytchek@gmail.com>
2017-11-15 21:30:25 +0400

    Disable newly created event taps and filter received events by original mask 🤯

--- source/Observer/Event/Observer.Event.Handler.Carbon.swift
+++ source/Observer/Event/Observer.Event.Handler.Carbon.swift
@@ -9,35 +9,38 @@ extension EventObserver.Handler
         open class Definition: ObserverHandlerDefinition
         {
             public init?(mask: CGEventMask, location: CGEventTapLocation, placement: CGEventTapPlacement, options: CGEventTapOptions, handler: @escaping Signature) {
-                let handlerPointer: UnsafeMutablePointer<Signature> = UnsafeMutablePointer.allocate(capacity: 1)
-                handlerPointer.initialize(to: handler)
+
+                // We may receive events that we didn't ask for, like null, tapDisabledByTimeout or tapDisabledByUserInput. To avoid it we must check
+                // that event mask matches the specified, kudos to https://bugs.swift.org/browse/SR-4073.
+
+                let userInfo: UnsafeMutablePointer<Signature> = UnsafeMutablePointer.allocate(capacity: 1)
+                userInfo.initialize(to: { mask & CGEventMask(1 << $0.type.rawValue) > 0 ? handler($0) : $0 })
 
                 let callback: CGEventTapCallBack = { (proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, handler: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? in
-                    if let event: CGEvent = UnsafeMutablePointer<Signature>(OpaquePointer(handler!)).pointee(event) {
-                        return Unmanaged.passUnretained(event)
-                    } else {
-                        return nil
-                    }
+                    return UnsafeMutablePointer<Signature>(OpaquePointer(handler!)).pointee(event).map({ Unmanaged.passUnretained($0) })
                 }
 
-                guard let tap: CFMachPort = CGEvent.tapCreate(tap: location, place: placement, options: options, eventsOfInterest: mask, callback: callback, userInfo: handlerPointer) else {
-                    return nil
-                }
+                // Tap should be initially disabled – it shouldn't play any role unless it's added to a loop, frankly it never did until recent,
+                // but it messes up event flow and sometimes events get blocked, for example, when adding inactive mouse down observer.
+
+                guard let tap: CFMachPort = CGEvent.tapCreate(tap: location, place: placement, options: options, eventsOfInterest: mask, callback: callback, userInfo: userInfo) else { return nil }
+                CGEvent.tapEnable(tap: tap, enable: false)
 
                 self.loop = CFRunLoopGetCurrent()
                 self.source = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
                 self.tap = tap
+                self.userInfo = userInfo
 
                 self.mask = mask
                 self.location = location
                 self.placement = placement
                 self.options = options
                 self.handler = handler
-
             }
 
             deinit {
                 self.deactivate()
+                self.userInfo.deallocate(capacity: 1)
             }
 
             // MARK: -
@@ -51,23 +54,25 @@ extension EventObserver.Handler
             private let loop: CFRunLoop
             private let source: CFRunLoopSource
             private let tap: CFMachPort
+            private let userInfo: UnsafeMutablePointer<Signature>
 
             // MARK: -
 
-            open private(set) var active: Bool = false
+            open var active: Bool {
+                return CGEvent.tapIsEnabled(tap: self.tap) && CFRunLoopContainsSource(self.loop, self.source, .commonModes)
+            }
 
             @discardableResult open func activate(_ newValue: Bool = true) -> Self {
                 if newValue == self.active { return self }
 
                 if newValue {
-                    CFRunLoopAddSource(self.loop, self.source, CFRunLoopMode.commonModes)
+                    CFRunLoopAddSource(self.loop, self.source, .commonModes)
                     CGEvent.tapEnable(tap: self.tap, enable: true)
                 } else {
                     CGEvent.tapEnable(tap: self.tap, enable: false)
-                    CFRunLoopRemoveSource(self.loop, self.source, CFRunLoopMode.commonModes)
+                    CFRunLoopRemoveSource(self.loop, self.source, .commonModes)
                 }
 
-                self.active = newValue
                 return self
             }
 
