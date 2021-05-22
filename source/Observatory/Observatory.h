728dd685a820812f3f306a233914b6ea1b48fb23
Ian Bytchek <ianbytchek@gmail.com>
2021-05-22 22:25:45 +0400

    Update and simplify xcconfigs usage

--- source/Observatory/Observatory.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for Observatory.
-FOUNDATION_EXPORT double ObservatoryVersionNumber;
-
-//! Project version string for Observatory.
-FOUNDATION_EXPORT const unsigned char ObservatoryVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Observatory/PublicHeader.h>
